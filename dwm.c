490cb88e0216466442ed1b96ad9ef5badf6a1596
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2020-04-12 18:31:56 -0300

    [fix] systray patch working with alpha patch

--- dwm.c
+++ dwm.c
@@ -2335,7 +2335,9 @@ updatesystray(void)
 	XMapSubwindows(dpy, systray->win);
 	/* redraw background */
 	XSetForeground(dpy, drw->gc, scheme[SchemeNorm][ColBg].pixel);
-	XFillRectangle(dpy, systray->win, drw->gc, 0, 0, w, bh);
+	XFillRectangle(dpy, systray->win, XCreateGC(dpy, root, 0 , NULL), 0, 0, w, bh);
+
+
 	XSync(dpy, False);
 }
 
